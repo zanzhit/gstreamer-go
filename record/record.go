57cf2d15a8c0f2cec293b2a46b96dfaf649b8e12
zanzhit <zangit@yandex.ru>
2024-02-03 17:05:47 +0300

    Добавить комментарии

--- record/record.go
+++ record/record.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 )
 
+// To work with json/txt files.
+// Parses an array of data and starts writing it.
 func Record(done chan struct{}, in Input, wg *sync.WaitGroup) {
 	var commands []*exec.Cmd
 
@@ -18,10 +20,11 @@ func Record(done chan struct{}, in Input, wg *sync.WaitGroup) {
 		}
 	}
 
-	go stopRecord(&commands, done, wg)
+	go StopRecord(&commands, done, wg)
 }
 
-func stopRecord(commands *[]*exec.Cmd, done chan struct{}, wg *sync.WaitGroup) {
+// Waits for a signal from the channel and then stops all recordings
+func StopRecord(commands *[]*exec.Cmd, done chan struct{}, wg *sync.WaitGroup) {
 	<-done
 	defer wg.Done()
 
