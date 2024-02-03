9c09e163b15a62c6e145e458be6933bc9a9f59b1
zanzhit <zangit@yandex.ru>
2024-02-03 17:17:15 +0300

    Добавить комментарии функциям

--- output/output.go
+++ output/output.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// Generates the name of the output file.
 func NameGenerate(i int) string {
 	recordTime := time.Now().Format("20060102_150405")
 	outputName := fmt.Sprintf("stream_%d_%s.mkv", i+1, recordTime)
